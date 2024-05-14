0917051ae83f40ab656c33a9f83668349be4181d
Vitoria2525 <torito2845828@gmail.com>
2024-05-14 18:37:28 +0300

    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .yml

--- project.py
+++ project.py
@@ -25,6 +25,8 @@ def write_data(data, file_path, file_format):
     with open(file_path, "w") as file:
         if file_format == "json":
             json.dump(data, file)
+        elif file_format == "yml" or file_format == "yaml":
+            yaml.dump(data, file)
         else:
             raise ValueError("Unsupported file format")
 
