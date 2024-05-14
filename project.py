5a91fec9aa5d364526df0f9394091c56255346ba
Vitoria2525 <torito2845828@gmail.com>
2024-05-14 18:30:13 +0300

    wczytywanie do obiektu z pliku .json i weryfikacja poprawności składni pliku

--- project.py
+++ project.py
@@ -1,4 +1,18 @@
 import argparse
+import json
+
+def read_data(file_path, file_format):
+    with open(file_path, "r") as file:
+        if file_format == "json":
+            try:
+                data = json.load(file)
+                return data
+            except json.JSONDecodeError:
+                print("Invalid JSON format or file does not exist.")
+                return None
+        else:
+            print("Unsupported file format.")
+            return None
 
 def main():
     parser = argparse.ArgumentParser(description="Convert data between .xml, .json, and .yml formats")
